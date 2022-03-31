a71387f4678118ed70568794ec19a91c9c27fde2
DanielDobromylskyj <90842024+DanielDobromylskyj@users.noreply.github.com>
2022-03-31 14:17:30 +0100

    Basic Image Maker / Viewer
    not to much just can make and display / store images

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ class OperatingSystem():
         for i in range(RAM_Amount):
             self.RAM.append([0, 0, 0, 0, 0, 0, 0, 0])
 
+        # Ready Display
+        self.DisplayClear()
+
+    def Wait(self):
+        input()
+
     def UserInput(self, text = "", Return = True, letter_case = None):
         raw_in = input(text)
 
@@ -34,8 +40,18 @@ class OperatingSystem():
     def DisplayText(self, text):
         print(text)
 
-    def DisplayImage(self, image):
-        print("Not Done - ", image)
+    def DisplayImage(self, img):
+        row_number = 0
+        for row in img:
+            row_data = ""
+            for col in row:
+                row_data = row_data + " " + col + " "
+            print(row_data)
+            row_number += 1
+
+    def DisplayClear(self):
+        for i in range(60):
+            print("")
 
 
     # ---   Storage   ---
@@ -56,31 +72,31 @@ class OperatingSystem():
         else:
             print("Value Is Not A List Of Bytes. Your Type: ", type(value))
 
-    def Store_Long(self, location, value):
+    def Store_Long(self, location, value, Type = "binlist"):
         # Check If That Location Is Valid
-        if location <= self.Secoundary:
+        if int(location) <= self.Secoundary:
             # Check If Its Valid Data
-            if type(value) == list:
-                if len(value) == 8:
-                    try:
-                        location_file = open("Storage/" + str(location) + ".txt", "x")
-                    except FileExistsError:
-                        location_file = open("Storage/" + str(location) + ".txt", "w")
+            try:
+                location_file = open("Storage/" + str(location) + ".txt", "x")
+            except FileExistsError:
+                    location_file = open("Storage/" + str(location) + ".txt", "w")
 
-                    # Turn List into string / storable data
-                    store = ""
-                    for Val in value:
-                        store = store + str(Val)
-
-                    location_file.write(store)
-                    location_file.close()
-
-                else:
-                    print("Max Amount Of Bits Per Value Is 8. Min Amount Of Bits Per Value Is 8. You Inputed: ",
-                          len(value))
+            # Turn List into string / storable data
+            if Type == "binlist":
+                store = "&!!"
+                for Val in value:
+                    store = store + str(Val)
+            elif Type == "image1":
+                store = "!&!1" + str(value)
             else:
-                print("Value Is Not A List Of Bytes. Your Type: ", type(value))
+                print("Invalid Data Type. Asumming its A List")
+                store = "&!!"
+                for Val in value:
+                    store = store + str(Val)
 
+
+            location_file.write(store)
+            location_file.close()
         else:
             print("Invalid Location - Not Found")
 
@@ -94,22 +110,123 @@ class OperatingSystem():
             file = open("Storage/" + str(location) + ".txt", "r")
             value = file.read()
             file.close()
-            list_value = []
-            for chr in value:
-                list_value.append(int(chr))
 
-            return list_value
+            Type = value[:3]
+
+            # 'binary' Text File Using 1 List
+            if Type == "&!!":
+                list_value = []
+                Data = value[3:]
+                for chr in Data:
+                    list_value.append(int(chr))
+
+                return list_value
+
+            # Image Format 1
+            if Type == "!&!": # Every 25 Chars is a new thing
+                if value[:4] == "!&!1":
+                    Data = value[4:]
+                    import ast
+                    return ast.literal_eval(Data)
+
+
+
         except FileNotFoundError:
             return None
 
 
 
+
+
+
+
+# Basic Image Program
+class ImageMaker():
+    def __init__(self, OS): # Basic Stuff That Needs Defining.
+        self.OS = OS
+        self.scale = ['$', '@', 'B', '%', '8', '&', 'W', 'M', '#', '*', 'o', 'a', 'h', 'k', 'b', 'd', 'p', 'q', 'w', 'm', 'Z', 'O', '0', 'Q', 'L', 'C', 'J', 'U', 'Y', 'X', 'z', 'c', 'v', 'u', 'n', 'x', 'r', 'j', 'f', 't', '/', '\\', '|', '(', ')', '1', '{', '}', '[', ']', '?', '-', '_', '+', '~', '<', '>', 'i', '!', 'l', 'I', ';', ':', ',', '"', '^', '`', "'", '.', ' ']
+
+    def open(self): # Program Code
+        self.alpha = ["A","B","C","D","E"]
+        self.Current_image = [
+            [" ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " "],
+            [" ", " ", " ", " ", " "]
+        ]
+
+        def Update():
+            self.OS.DisplayClear()
+            print("-- Image Maker v0.1 --")
+            print("")
+            print("* : -1- -2- -3- -4- -5-")
+            row_number = 0
+            for row in self.Current_image:
+                row_data = self.alpha[row_number] + " : |"
+                for col in row:
+                    row_data = row_data + " " + col + " |"
+                print(row_data)
+                row_number += 1
+
+        def Save():
+            self.OS.DisplayClear()
+            location = self.OS.UserInput(text="Location: ")
+            self.OS.Store_Long(location, self.Current_image, Type="image1")
+
+
+
+        Update()
+
+
+        while True:
+            print("")
+            UserIn = self.OS.UserInput(text="GridLetter GridNumber@@character OR exit / save: ", letter_case='upper')
+
+            if UserIn == 'EXIT':
+                break
+
+
+            elif UserIn == 'SAVE':
+                Save()
+                self.OS.DisplayClear()
+                break
+
+            else:
+                try:
+                    gridref, char = UserIn.split("@@")
+                    if char in self.scale:
+                        Letter, col = gridref.split(" ")
+                        col = int(col) - 1 # Make it proper for list
+                        row = self.alpha.index(Letter)
+
+                        # Change List
+                        self.Current_image[row].pop(col)
+                        self.Current_image[row].insert(col, str(char))
+                        # Update Screen
+                        Update()
+
+
+                except Exception as e:
+                    print("Bad Input | Eg: A 2@@#")
+                    pass
+
+
+
+
+
+
+
+
+
 OS = OperatingSystem(100, 50)
+imgmk = ImageMaker(OS)
 
-OS.UserInput(text="press enter to start", Return=False)
-OS.Store_RAM(41, [1, 0, 0, 1, 0, 1, 0, 0])
-OS.Store_Long(21, [0, 1, 1, 0, 1, 0, 1, 1])
+OS.UserInput(text="press enter to start demo", Return=False)
 
-print(OS.Get_RAM(41))
-print(OS.Get_Long(21))
-print(OS.Get_Long(11))
+img = ImageMaker(OS)
+img.open()
+
+OS.DisplayImage(OS.Get_Long(3))
+
+OS.Wait()
